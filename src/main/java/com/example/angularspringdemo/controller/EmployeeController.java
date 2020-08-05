5b48dae75be6c99a6badd27325809110749280aa
SugLearn <saranya.sakthivel06@gmail.com>
2020-08-05 13:12:00 +0530

    Added Exception handler, logging framework and Junit testing

--- src/main/java/com/example/angularspringdemo/controller/EmployeeController.java
+++ src/main/java/com/example/angularspringdemo/controller/EmployeeController.java
@@ -4,7 +4,11 @@ package com.example.angularspringdemo.controller;
 import com.example.angularspringdemo.bean.AuthenticationBean;
 import com.example.angularspringdemo.model.Employee;
 import com.example.angularspringdemo.repository.EmployeeRepository;
+import com.example.angularspringdemo.service.EmployeeService;
 import com.sun.org.apache.xpath.internal.operations.Bool;
+import org.apache.velocity.exception.ResourceNotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,13 +16,17 @@ import com.example.angularspringdemo.bean.AuthenticationBean;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 @CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/v1")
 public class EmployeeController {
     @Autowired
-    private EmployeeRepository employeeRepository;
+    private EmployeeService employeeService;
+
+    Logger logger = LoggerFactory.getLogger(EmployeeController.class);
+
 
     @GetMapping(path = "/basicauth")
     public AuthenticationBean helloWorldBean() {
@@ -27,38 +35,49 @@ public class EmployeeController {
 
     @GetMapping("/getEmployees")
     public List<Employee> getAllEmployees(){
-        return employeeRepository.findAll();
+        logger.trace("Get Employees");
+        return employeeService.findAll();
+
     }
 
     @PostMapping("/addEmployees")
-    public Employee createEmployee( @RequestBody Employee employee){
-        return employeeRepository.save(employee);
+    public ResponseEntity<Employee> createEmployee( @RequestBody Employee employee){
+        logger.trace("posted Employees");
+        return ResponseEntity.ok(employeeService.createOrUpdate(employee));
     }
 
     @PutMapping("/updateEmployee/{id}")
-    public ResponseEntity<Employee> updateEmployee(@PathVariable(value = "id") Long employeeId, @RequestBody Employee employeeDetails){
-        Employee employee = employeeRepository.findById(employeeId).orElse(null);
+    public ResponseEntity<Employee> updateEmployee(@PathVariable(value = "id") Long employeeId, @RequestBody Employee employeeDetails)
+            throws ResourceNotFoundException{
+        Employee employee = employeeService.findById(employeeId)
+                        .orElseThrow(() -> new ResourceNotFoundException("Employee not found for this id :: " + employeeId));
         employee.setFirstName(employeeDetails.getFirstName());
         employee.setLastName(employeeDetails.getLastName());
         employee.setEmailId(employeeDetails.getEmailId());
         employee.setMobile(employeeDetails.getMobile());
         employee.setAddress(employeeDetails.getAddress());
-        final Employee updateEmployee = employeeRepository.save(employee);
+       final Employee updateEmployee = employeeService.createOrUpdate(employee);
+        logger.trace("updated the Employees");
         return ResponseEntity.ok(updateEmployee);
     }
 
     @DeleteMapping("deleteEmployee/{id}")
-    public Map<String, Boolean> deleteEmployee(@PathVariable(value = "id") Long employeeId){
-        Employee employee = employeeRepository.findById(employeeId).orElse(null);
-        employeeRepository.delete(employee);
+    public Map<String, Boolean> deleteEmployee(@PathVariable(value = "id") Long employeeId)
+            throws ResourceNotFoundException{
+        Employee emp = employeeService.findById(employeeId)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee not found for this id :: " + employeeId));
+        employeeService.deleteById(employeeId);
         Map<String, Boolean> response = new HashMap<>();
         response.put("deleted", Boolean.TRUE);
+        logger.trace("deleted Employee");
         return response;
     }
 
     @GetMapping("/getEmployeeById/{id}")
-    public ResponseEntity<Employee> getEmployeeById(@PathVariable(value = "id") Long employeeId){
-        Employee employee = employeeRepository.findById(employeeId).orElse(null);
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable(value = "id") Long employeeId) throws ResourceNotFoundException {
+        Employee employee = employeeService.findById(employeeId)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee not found this id-"+ employeeId));
+        logger.trace("GetById Employee");
         return ResponseEntity.ok().body(employee);
 
     }
--- src/test/java/com/example/angularspringdemo/AngularSpringDemoApplicationTests.java
+++ src/test/java/com/example/angularspringdemo/AngularSpringDemoApplicationTests.java
@@ -1,8 +1,13 @@
 package com.example.angularspringdemo;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 class AngularSpringDemoApplicationTests {
 
